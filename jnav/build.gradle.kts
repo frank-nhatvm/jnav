bb94efe552cb9174035f91057da7efef7b76f208
frank <nhat.thtb@gmail.com>
2025-09-21 14:16:03 +0700

    configure signing block

--- jnav/build.gradle.kts
+++ jnav/build.gradle.kts
@@ -75,6 +75,9 @@ publishing{
 }
 
 signing {
-    useGpgCmd()
+    useInMemoryPgpKeys(
+        System.getenv("GPG_PRIVATE_KEY"),
+        System.getenv("GPG_PASSPHRASE")
+    )
     sign(publishing.publications["jnav"])
 }
