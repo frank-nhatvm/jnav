4e7ceb44b0e55dd8cba0b29e3515047fe74618c1
frank <nhat.thtb@gmail.com>
2025-09-21 13:59:37 +0700

    use GPG for singing

--- jnav/build.gradle.kts
+++ jnav/build.gradle.kts
@@ -71,21 +71,10 @@ publishing{
                 password = findProperty("mavenCentralPassword") as String? ?: System.getenv("MAVEN_CENTRAL_PASSWORD")
             }
         }
-
-        maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-            credentials {
-                username = "frank_vu"
-                password = "Frank932714@"
-            }
-        }
     }
 }
 
 signing {
+    useGpgCmd()
     sign(publishing.publications["jnav"])
 }
-
-// publish library manually : ./gradlew publishJnavPublicationToMavenRepository
