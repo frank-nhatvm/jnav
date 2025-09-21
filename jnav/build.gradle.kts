c63b6dd1fd793642037cf8c525279a7d50ca2310
frank <nhat.thtb@gmail.com>
2025-09-21 15:42:36 +0700

    new Publish plugin

--- jnav/build.gradle.kts
+++ jnav/build.gradle.kts
@@ -1,83 +1,58 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
-    id("maven-publish")
-    signing
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 group = "com.fatherofapps.jnav"
-version = "2.0.0"
+version = "2.0.2"
 
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
 
-kotlin{
+kotlin {
     jvmToolchain(11)
 }
 
 
 dependencies {
-    implementation ("com.google.devtools.ksp:symbol-processing-api:2.2.20-2.0.2")
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.2.20-2.0.2")
     implementation("com.squareup:kotlinpoet:2.2.0")
     implementation("com.squareup:kotlinpoet-ksp:2.2.0")
 }
 
-
-publishing{
-    val sdkVersion  = project.version.toString()
-    publications{
-        create<MavenPublication>("jnav"){
-            groupId = "com.fatherofapps"
-            artifactId = "jnav"
-            version = sdkVersion
-            from(components["java"])
-            pom{
-                name = "JNav"
-                description = "A library to generate route for Jetpack Compose Navigation by using KSP"
-                url = "https://github.com/frank-nhatvm/jnav"
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "frank_vu"
-                        name = "Frank Vu"
-                        email = "nhat.thtb@gmail.com"
-                    }
-                }
-                scm {
-                    connection = "scm:git@github.com:frank-nhatvm/jnav.git"
-                    developerConnection = "scm:git@github.com:frank-nhatvm/jnav.git"
-                    url = "https://github.com/frank-nhatvm/jnav"
-                }
-            }
-        }
-    }
-    repositories {
-        maven {
-            name = "centralStaging"
-            url = uri("https://central.sonatype.com/api/v1/publisher/deployments")
-            credentials {
-                username = findProperty("mavenCentralUsername") as String? ?: System.getenv("MAVEN_CENTRAL_USERNAME")
-                password = findProperty("mavenCentralPassword") as String? ?: System.getenv("MAVEN_CENTRAL_PASSWORD")
-            }
-        }
-    }
-}
-
-signing {
-    useInMemoryPgpKeys(
-        System.getenv("GPG_PRIVATE_KEY"),
-        System.getenv("GPG_PASSPHRASE")
+mavenPublishing {
+    val sdkVersion = project.version.toString()
+    coordinates(
+        groupId = "com.fatherofapps.jnav",
+        artifactId = "jnav",
+        version = sdkVersion
     )
-    sign(publishing.publications["jnav"])
+    pom {
+        name = "JNav"
+        description =
+            "A library to generate route for Jetpack Compose Navigation by using KSP"
+        url = "https://github.com/frank-nhatvm/jnav"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "frank_vu"
+                name = "Frank Vu"
+                email = "nhat.thtb@gmail.com"
+            }
+        }
+        scm {
+            connection = "scm:git@github.com:frank-nhatvm/jnav.git"
+            developerConnection = "scm:git@github.com:frank-nhatvm/jnav.git"
+            url = "https://github.com/frank-nhatvm/jnav"
+        }
+    }
 }
